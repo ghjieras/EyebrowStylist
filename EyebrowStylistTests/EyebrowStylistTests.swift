b446cc3d9dd493a960618f40437a7a55423da51e
Wade_test <ghjieras@gmail.com>
2025-06-19 14:02:59 +0800

    Initial Commit

---
+++ EyebrowStylistTests/EyebrowStylistTests.swift
@@ -0,0 +1,17 @@
+//
+//  EyebrowStylistTests.swift
+//  EyebrowStylistTests
+//
+//  Created by 呂念浯 on 2025/6/19.
+//
+
+import Testing
+@testable import EyebrowStylist
+
+struct EyebrowStylistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
